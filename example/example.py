8f733fc8c52f31dd3cf44f836401349926c40b1c
uki <hello@uki.dev>
2022-12-18 04:15:45 +1100

    update example

--- example/example.py
+++ example/example.py
@@ -1,27 +1,36 @@
-import cadquery
+import cadquery as cq
 
-u = 2
-h = 2
-lh = 4.1 / 2
-th = 1.17 / 2
+sphere = cq.Workplane().sphere(5)
+base = (cq.Workplane(origin=(0,0,-2))
+    .box(12,12,10)
+    .cut(sphere)
+    .edges("|Z")
+    .fillet(2)
+)
+sphere_face = base.faces(">>X[2] and (not |Z) and (not |Y)").val()
+base = (base
+    .faces("<Z")
+    .workplane()
+    .circle(2)
+    .extrude(10)
+)
 
-# body
-keycap = cadquery.Workplane('XZ').box(16 * u, 16, h).edges('|Y').fillet(2)
-# stem mount
-keycap = keycap.faces('<Y').polyline([
-    (-lh, 0),
-    (-lh, th),
-    (-th, th),
-    (-th, lh),
-    (th, lh),
-    (th, th),
-    (lh, th),
-    (lh, 0),
-]).mirrorX().extrude(h / 2, 'cut')
+shaft = (cq.Workplane()
+    .sphere(4.5)
+    .circle(1.5)
+    .extrude(20)
+)
 
-assembly = cadquery.Assembly(metadata = {
-    'material': 'acrylic',
-})
-assembly.add(keycap)
+spherical_joint = (base.union(shaft)
+    .faces(">X")
+    .workplane(centerOption="CenterOfMass")
+    .move(0,4)
+    .slot2D(10,2,90)
+    .cutBlind(sphere_face)
+    .workplane(offset=10)
+    .move(0,2)
+    .circle(0.9)
+    .extrude("next")
+)
 
-result = assembly
+result = spherical_joint
