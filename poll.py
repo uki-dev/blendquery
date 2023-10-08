7724d905c6d1c3846ca48fe21d23ed342a3beaf3
uki <hello@uki.dev>
2023-10-08 11:14:16 +1100

    ignore `ReferenceError` when polling text

--- poll.py
+++ poll.py
@@ -15,26 +15,30 @@ def watch_for_text_changes(
 
     # TODO: @uki-dev can we pull this outside of this function?
     def timer():
-        nonlocal last_string, last_modified
-        # Support filepath of text changing
-        abs_filepath = bpy.path.abspath(text.filepath, library=text.library)
-        # TODO: Find a way to avoid:
-        #           - Marking the blend file as having unsaved changes due to hot reloading external file
-        #           - Overwriting legitimate local changes when external file is modified
-        #       Currently, because we want to load this in the background without `bpy.ops.text.reload`,
-        #       we have to read the file from disk natively, and overwrite the texts contents with `from_string`.
-        #       This subsequently marks the text as being dirty and modified.
-        if not text.is_in_memory:
-            # If text is external and it has no local changes, check if it has been modified and `revert` to reload from disk
-            modified = os.path.getmtime(abs_filepath)
-            if modified > last_modified:
-                last_modified = modified
-                reload_text(text)
+        # Wrap in try/except as sometimes, perhaps due to the fact that we are 'reloading' the text, a `ReferenceError: StructRNA of type Text has been removed` will be thrown
+        try:
+            nonlocal last_string, last_modified
+            # Support filepath of text changing
+            abs_filepath = bpy.path.abspath(text.filepath, library=text.library)
+            # TODO: Find a way to avoid:
+            #           - Marking the blend file as having unsaved changes due to hot reloading external file
+            #           - Overwriting legitimate local changes when external file is modified
+            #       Currently, because we want to load this in the background without `bpy.ops.text.reload`,
+            #       we have to read the file from disk natively, and overwrite the texts contents with `from_string`.
+            #       This subsequently marks the text as being dirty and modified.
+            if not text.is_in_memory:
+                # If text is external and it has no local changes, check if it has been modified and `revert` to reload from disk
+                modified = os.path.getmtime(abs_filepath)
+                if modified > last_modified:
+                    last_modified = modified
+                    reload_text(text)
 
-        string = text.as_string()
-        if string != last_string:
-            last_string = string
-            callback()
+            string = text.as_string()
+            if string != last_string:
+                last_string = string
+                callback()
+        except:
+            pass
 
         return poll_rate
 
@@ -51,5 +55,4 @@ def reload_text(text: bpy.types.Text):
         raise ValueError(f"Text object ('{text.name}') is not an external file.")
     abs_filepath = bpy.path.abspath(text.filepath, library=text.library)
     with open(abs_filepath) as file:
-        print("reload text: " + abs_filepath)
         text.from_string(file.read())
